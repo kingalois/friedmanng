8ae3262dbb5714d99f14a8bd690edd979bf1e625
kingalois <alois@muehleder.at>
2017-12-27 21:08:10 +0100

    #4 create first test class

---
+++ at.muehleder.friedmanng.data/test/at/muehleder/friedmanng/data/GraveOwnerTest.java
@@ -0,0 +1,12 @@
+package at.muehleder.friedmanng.data;
+
+import org.junit.Test;
+
+public class GraveOwnerTest {
+	
+	@Test
+	public void test() {
+		
+	}
+
+}
---
+++ at.muehleder.friedmanng.data/test/at/muehleder/friedmanng/data/test.java
@@ -0,0 +1,14 @@
+package at.muehleder.friedmanng.data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
