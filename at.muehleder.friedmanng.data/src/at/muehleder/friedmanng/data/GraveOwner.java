17c4707bd2b2a572665dea37a102bbee401b9824
flomue99 <34889526+flomue99@users.noreply.github.com>
2017-12-27 23:57:21 +0100

    #5
    Added all dates

--- at.muehleder.friedmanng.data/src/at/muehleder/friedmanng/data/GraveOwner.java
+++ at.muehleder.friedmanng.data/src/at/muehleder/friedmanng/data/GraveOwner.java
@@ -4,8 +4,12 @@ public class GraveOwner {
 	
 	private String firstName;
 	private String familyName;
+	private String street;
+	private int housenumber;
+	private int postalCode;
+	private String town;
 	
-	//firstName
+	//FirstName
 	public void setFirstName(String name) {
 		firstName = name;
 	}
@@ -21,4 +25,37 @@ public class GraveOwner {
 	public String getFamilyName() {
 		return familyName;
 	}
+	//address
+	public void setStreetName(String streetName) {
+		street = streetName;
+	}
+	
+	public String getStreetName() {
+		return street;
+	}
+	//HouseNumber
+	public void setHouseNumber(int streetNumber) {
+		housenumber = streetNumber;
+	}
+	
+	public int getHouseNumber() {
+		return housenumber;
+	}
+	//PostalCode
+	public void setPostalCode(int postCode) {
+		postalCode = postCode;	
+	}
+	
+	public int getPostalCode() {
+		return postalCode;
+	}
+	//Town
+	public void setTown(String village) {
+		town = village;
+	}
+	
+	public String getTown() {
+		return town;
+	}
 }
+
